97dde463ba5b7949b2fbcba1fe533be52829ca4b
Yi Zhou <kickccat@hotmail.com>
2018-07-24 11:49:37 +0200

    cleanup code with constructure injection

--- Practice/SpringMVC/guess-the-number-game/console/src/main/java/io/sample/console/Main.java
+++ Practice/SpringMVC/guess-the-number-game/console/src/main/java/io/sample/console/Main.java
@@ -1,8 +1,6 @@
 package io.sample.console;
 
-import io.sample.config.AppConfig;
-import io.sample.MessageGenerator;
-import io.sample.NumberGenerator;
+import io.sample.config.GameConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -15,24 +13,8 @@ public class Main {
     public static void main(String[] args) {
         log.info("Guess the number game");
         
-        // Context
-        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        
-        // Get number generator bean from context
-        NumberGenerator numberGenerator = context.getBean(NumberGenerator.class);
-        
-        // Random number
-        int number = numberGenerator.next();
-        
-        // Log generated number
-        log.info("number = {}", number);
-        
-        // Get message generator bean from context
-        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
-        
-        // Log generated message
-        log.info("getMainMessage = {}", messageGenerator.getMainMessage());
-        log.info("getResultMessage = {}", messageGenerator.getResultMessage());
+        // Create context
+        ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(GameConfig.class);
         
         // Close context
         context.close();
--- Practice/SpringMVC/guess-the-number-game/core/src/main/java/io/sample/config/GameConfig.java
+++ Practice/SpringMVC/guess-the-number-game/core/src/main/java/io/sample/config/GameConfig.java
@@ -5,11 +5,13 @@ import io.sample.MaxNumber;
 import io.sample.MinNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:config/game.properties")
+@ComponentScan(basePackages = "io.sample")
 public class GameConfig {
     
     // == fields ==
