18b377064cf5ef38be728e66f3ffb2b94569a80b
Yi Zhou <kickccat@hotmail.com>
2018-10-05 17:40:45 +0200

    add Rabin Karp Algorithm

---
+++ Practice/AlgorithmsInJava/src/main/java/io/sample/rabinkarp/RabinKarp.java
@@ -0,0 +1,56 @@
+package io.sample.rabinkarp;
+
+/**
+ * Rabin-Karp Algorithm
+ * Using rolling hash function to compare the hashcode between pattern and array
+ */
+public class RabinKarp {
+    
+    private final int prime = 3;
+    
+    int search(char[] array, char[] pattern) {
+        int n = array.length;
+        int m = pattern.length;
+        int lastChar = n - m;
+        long patternHash = calculateHash(pattern, m);
+        long arrayHash = calculateHash(array, m);
+    
+        for (int i = 0; i <= lastChar; i++) {
+            if (patternHash == arrayHash && match(array, pattern, i)) {
+                return i;
+            }
+            if (i < lastChar) {
+                arrayHash = recalculateHash(arrayHash, array[i], array[i + m], m);
+            }
+        }
+        return -1;
+    }
+    
+    private boolean match(char[] array, char[] pattern, int index) {
+        for (int i = 0; i < pattern.length; i++) {
+            if (array[index + i] != pattern[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private long recalculateHash(long oldHash, char oldChar, char newChar, int hashSize) {
+        long hash = oldHash - charVal(oldChar);
+        hash /= prime;
+        hash += charVal(newChar) * Math.pow(prime, hashSize - 1);
+        return hash;
+    }
+    
+    private long calculateHash(char[] text, int hashSize) {
+        long hash = 0;
+        for (int i = 0; i < hashSize; i++) {
+            hash += charVal(text[i]) * Math.pow(prime, i);
+        }
+        return hash;
+    }
+    
+    private int charVal(char c) {
+        return c - 96;
+    }
+}
